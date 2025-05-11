e899ac3994d468612cf5a773904ae35f6c37e5f1
9osari <128437395+9osari@users.noreply.github.com>
2025-05-11 22:21:21 +0900

    feat: 열거형 학습

---
+++ src/enumeration/ex3/Grade.java
@@ -0,0 +1,17 @@
+package enumeration.ex3;
+
+import enumeration.ex2.ClassGrade;
+
+public enum Grade {
+    //원하는 상수 이름 나열
+    BASIC, GOLD, DIAMOND
+
+
+    //이거랑 같은 코드
+    /*public static final ClassGrade BASIC = new ClassGrade();
+    public static final ClassGrade GOLD = new ClassGrade();
+    public static final ClassGrade DIAMOND = new ClassGrade();
+
+    //private 생성자 추가
+    private ClassGrade() {}*/
+}
