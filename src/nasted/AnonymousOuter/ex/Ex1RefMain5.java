2d52e4ec91187a0dd1ea771856adaf161e7c9cfe
9osari <128437395+9osari@users.noreply.github.com>
2025-05-28 22:34:05 +0900

    add: 익명 클래스 활용3 (람다)

---
+++ src/nasted/AnonymousOuter/ex/Ex1RefMain5.java
@@ -0,0 +1,29 @@
+package nasted.AnonymousOuter.ex;
+
+import java.util.Random;
+
+public class Ex1RefMain5 {
+    public static void hello(Process process) {
+        System.out.println("프로그램 시작");
+
+        //여기서 받아야함
+        process.run();
+
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello 실행");
+        //익명 클래스
+        hello(() -> {
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위 = " + randomValue);
+        });
+
+        hello(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        });
+    }
+}
