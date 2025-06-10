53dff89811f375758b1d4e52d7345be3b8717bfb
9osari <128437395+9osari@users.noreply.github.com>
2025-06-10 22:40:06 +0900

    add: 예외처리 - 실습3

---
+++ src/exception/ex2/NetworkServiceV2_3.java
@@ -0,0 +1,22 @@
+package exception.ex2;
+
+
+public class NetworkServiceV2_3 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+
+        //NerworkClientV0을 생성하고 외부 서버 주소 전달
+        NetworkClientV2 client = new NetworkClientV2(address);
+        client.initError(data);
+
+        try {
+            client.connect();
+            client.send(data);
+            client.disconnect();
+        } catch (NerworkClientExceptionV2 e) {
+            System.out.println("[ERROR] " + e.getErrorCode() + " / [MESSAGE] " + e.getMessage());
+        }
+
+    }
+}
